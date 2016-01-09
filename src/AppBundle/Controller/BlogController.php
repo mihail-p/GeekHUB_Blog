684d1f1c952ad0016d5b554272e72a5e1537cd49
Mihail Pronyuk <mishaprk@gmail.com>
2016-01-09 22:27:33 +0200

    template bootstrap work

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/blog", name="indexBlog")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+
+        return $this->render('blog/index.html.twig', ['base_dir' => ''
+            ]);
+    }
+}
---
+++ src/AppBundle/Entity/Posts.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Entiy;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Posts
+{
+
+}
